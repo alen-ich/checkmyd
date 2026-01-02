95628edc010b6d44066837c1bea72008afea4a55
alen ichshanov <alen.ichshanov@chocotravel.com>
2026-01-02 17:54:43 +0500

    feat: added rater profile page

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import SignUpRater from "./pages/SignUp/SignUpRater";
 import Raters from "./pages/Raters/Raters";
 import UploadPhoto from "./pages/Upload/UploadPhoto";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import RaterProfile from "./pages/RaterProfile/RaterProfile";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
@@ -23,6 +24,7 @@ ReactDOM.createRoot(rootElement).render(
       <Route path="/raters" element={<Raters />} />
       <Route path="/upload" element={<UploadPhoto />} />
       <Route path="/stats" element={<Dashboard />} />
+      <Route path="/rater-profile" element={<RaterProfile />} />
     </Routes>
   </BrowserRouter>,
 );
